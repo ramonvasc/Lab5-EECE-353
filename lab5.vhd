aa44690a07383a50162c83849d79c368d4ee5f0c
ramonvasc <ramonvasc@gmail.com>
2014-03-17 20:45:00 -0700

    Moving all players
    All the players are moving

--- lab5.vhd
+++ lab5.vhd
@@ -32,7 +32,7 @@ architecture RTL of lab5 is
   signal y      : std_logic_vector(6 downto 0);
   signal colour : std_logic_vector(2 downto 0);
   signal plot   : std_logic; 
-  signal slow_clock: std_logic_vector (20 downto 0); 
+  signal slow_clock: std_logic_vector (18 downto 0); 
   signal variable_clock : std_logic;
   signal clock_selector : std_logic;
   signal XpositionB1  : integer range 0 to 159;
@@ -64,7 +64,7 @@ begin
              VGA_CLK   => VGA_CLK);
 				 
 process(CLOCK_50)
-  variable slow_clock_var: unsigned (20 downto 0);
+  variable slow_clock_var: unsigned (18 downto 0);
 begin
 	if (CLOCK_50'event and CLOCK_50 = '1') then
 	slow_clock_var:= unsigned (slow_clock);
@@ -89,18 +89,21 @@ process(variable_clock,KEY(3))
   variable colourVar : std_logic_vector(2 downto 0);
   variable countYB1 : integer range 0 to 119 := 30;
   variable countOldYB1 : integer range 0 to 119 := 30;
-  variable countYB2 : integer range 0 to 119 := 41; 
-  variable countYR1 : integer range 0 to 119 := 30; 
-  variable countYR2 : integer range 0 to 119 := 41;   
+  variable countYB2 : integer range 0 to 119 := 41;
+  variable countOldYB2 : integer range 0 to 119 := 41;
+  variable countYR1 : integer range 0 to 119 := 30;
+  variable countOldYR1 : integer range 0 to 119 := 30;
+  variable countYR2 : integer range 0 to 119 := 41; 
+  variable countOldYR2 : integer range 0 to 119 := 41;  
 begin		
 	if(KEY(3) = '0') then -- Reset
 		int_value := "00000";
-   elsif(variable_clock'event and variable_clock = '1') then -- each time a counter overflows, 1 bit is plotted on screen
-	if(KEY(0) = '0') then -- Each time Key(0) is pressed, a line is plotted
+   elsif(variable_clock'event and variable_clock = '1') then --uses the clock selected ,can be a fast or slow clock
+	if(KEY(0) = '0') then
 		int_value := "11111";
 	end if;
 	case int_value is
-		when "00000" =>
+		when "00000" => --restart the game
 		clock_selector <= '0';
 		if(countX > 159) then
 			countY := countY + 1;
@@ -148,7 +151,7 @@ begin
 		y <= std_logic_vector(to_unsigned(countY,7));
 		countYB1 := 30;
 		
-		when "00001" =>
+		when "00001" => --plot player blue 1
 		if(SW(0) = '1') then
 			countOldYB1 := countYB1 + 10;
 			if(countYB1 > 1) then
@@ -166,10 +169,82 @@ begin
 			y <= std_logic_vector(to_unsigned(countYB1,7));
 			int_value := "00010";
 			
-		when "00010" =>
+		when "00010" => --erase player blue 1
 		colourVar := "000";
 		x <= std_logic_vector(to_unsigned(XpositionB1,8));
 		y <= std_logic_vector(to_unsigned(countOldYB1,7));
+		int_value := "00011";
+		
+		when "00011" => --plot player blue 2
+		if(SW(1) = '1') then
+			countOldYB2 := countYB2 + 10;
+			if(countYB2 > 1) then
+			countYB2 := countYB2 - 1;
+			end if;		
+		elsif(SW(1) = '0') then	
+			countOldYB2 := countYB2 - 10;		
+			if(countYB2 < 118) then
+			countYB2 := countYB2 + 1;
+			end if;
+		end if;
+			colourVar := "001";
+			plot <= '1';
+			x <= std_logic_vector(to_unsigned(XpositionB2,8));
+			y <= std_logic_vector(to_unsigned(countYB2,7));
+			int_value := "00100";
+			
+		when "00100" => --erase player blue 2
+		colourVar := "000";
+		x <= std_logic_vector(to_unsigned(XpositionB2,8));
+		y <= std_logic_vector(to_unsigned(countOldYB2,7));
+		int_value := "00101";
+		
+		when "00101" => --plot player red 1
+		if(SW(2) = '1') then
+			countOldYR1 := countYR1 + 10;
+			if(countYR1 > 1) then
+			countYR1 := countYR1 - 1;
+			end if;		
+		elsif(SW(2) = '0') then	
+			countOldYR1 := countYR1 - 10;		
+			if(countYR1 < 118) then
+			countYR1 := countYR1 + 1;
+			end if;
+		end if;
+			colourVar := "100";
+			plot <= '1';
+			x <= std_logic_vector(to_unsigned(XpositionR1,8));
+			y <= std_logic_vector(to_unsigned(countYR1,7));
+			int_value := "00110";
+			
+		when "00110" => --erase player red 1
+		colourVar := "000";
+		x <= std_logic_vector(to_unsigned(XpositionR1,8));
+		y <= std_logic_vector(to_unsigned(countOldYR1,7));
+		int_value := "00111";
+		
+		when "00111" => --plot player red 2
+		if(SW(3) = '1') then
+			countOldYR2 := countYR2 + 10;
+			if(countYR2 > 1) then
+			countYR2 := countYR2 - 1;
+			end if;		
+		elsif(SW(3) = '0') then	
+			countOldYR2 := countYR2 - 10;		
+			if(countYR2 < 118) then
+			countYR2 := countYR2 + 1;
+			end if;
+		end if;
+			colourVar := "100";
+			plot <= '1';
+			x <= std_logic_vector(to_unsigned(XpositionR2,8));
+			y <= std_logic_vector(to_unsigned(countYR2,7));
+			int_value := "01000";
+			
+		when "01000" => --erase player red 2
+		colourVar := "000";
+		x <= std_logic_vector(to_unsigned(XpositionR2,8));
+		y <= std_logic_vector(to_unsigned(countOldYR2,7));
 		int_value := "00001";
 		
 		when others =>
